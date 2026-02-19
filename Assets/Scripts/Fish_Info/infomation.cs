a5b9b37080f74088f57673f3cdfbc2adf0aee1ac
japan0101 <japan.pon11@gmail.com>
2026-02-19 15:49:37 +0700

    Revert "Merge branch 'main' of https://github.com/hinatsu13/food"
    This reverts commit 8e154db655b70ac16e7e91f2d5fefc555c1b39df, reversing
    changes made to 81269b916f3ae34e02afec7892cf084ec7eef8ad.

--- Assets/Scripts/Fish_Info/infomation.cs
+++ Assets/Scripts/Fish_Info/infomation.cs
@@ -1,35 +1,46 @@
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class infomation : MonoBehaviour
 {
-    public RectTransform targetButtonRect;
+    public TMP_Text fish_name;
+    public UnityEngine.UI.Image fish_anime;
+    public TMP_Text fish_descrip;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //info.text = "what the fox say";
-        gameObject.SetActive(false);
     }
 
-    public void OpenImage()
+    public void ShowInfomation_fish1()
     {
-        gameObject.SetActive(true);
+        fish_name.text = "???1";
+        fish_anime.color = Color.blue;
+        fish_descrip.text = "info about fish1";
     }
 
-    public void CloseImage()
+    public void ShowInfomation_fish2()
     {
-        gameObject.SetActive(false);
+        fish_name.text = "fish2";
+        fish_anime.color = Color.pink;
+        fish_descrip.text = "info about fish2";
     }
 
-    public void ChangeButtonLocation()
+    public void ShowInfomation_fish3()
     {
-        targetButtonRect.anchoredPosition = new Vector2(570, 645);
+        fish_name.text = "fish3";
+        fish_anime.color = Color.green;
+        fish_descrip.text = "info about fish3";
     }
 
-    public void ResetLocation()
+    public void ShowInfomation_fish4()
     {
-        targetButtonRect.anchoredPosition = new Vector2(970, 645);
+        fish_name.text = "fish4";
+        fish_anime.color = Color.yellow;
+        fish_descrip.text = "info about fish4";
     }
+
 
     // Update is called once per frame
     void Update()
