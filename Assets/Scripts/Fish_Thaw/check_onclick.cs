5a6efab6986827f302bbe40de85b5358c9950173
Mew_miruku <naegimakoto18147555@gmail.com>
2026-03-10 21:06:43 +0700

    animate_fishthaw

---
+++ Assets/Scripts/Fish_Thaw/check_onclick.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class check_onclick : MonoBehaviour
+{
+    [SerializeField] int click;
+    int click_count;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void OnButtonClick()
+    {
+        click_count++; // Increment the counter every time the button is clicked
+
+        if (click_count == click)
+        {
+            ShowObject();
+        }
+    }
+
+    void ShowObject()
+    {
+        gameObject.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
