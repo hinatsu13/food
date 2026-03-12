d5f2bf0ee84653e55a764c10a18c2e8fa97db47d
Mew_miruku <naegimakoto18147555@gmail.com>
2026-03-12 20:53:17 +0700

    pausegame

---
+++ Assets/Scripts/setting/setting.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class setting : MonoBehaviour
+{
+    private bool isPaused = false;
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
