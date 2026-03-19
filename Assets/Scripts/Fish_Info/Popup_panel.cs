ef772ef248b4beedcea54694dc507355aa5831e7
Mew_miruku <109953842+hinatsu13@users.noreply.github.com>
2026-03-19 12:19:37 +0700

    UI update

--- Assets/Scripts/Fish_Info/Popup_panel.cs
+++ Assets/Scripts/Fish_Info/Popup_panel.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class Popup_panel : MonoBehaviour
 {
+    [SerializeField] public bool popup = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject.SetActive(false);
+        gameObject.SetActive(popup);
     }
 
     public void OpenPanel()
