8202ecfebf00e3a3d9319666e13e91c103d819dc
japan0101 <japan.pon11@gmail.com>
2026-04-11 14:15:38 +0700

    Reapply "Add animation clips for filling oil"
    This reverts commit b646e2dbc4cc21d59172ff369d07c79691f63a28.

--- Assets/Scripts/Fish_Pakaging/FishPakagingManager.cs
+++ Assets/Scripts/Fish_Pakaging/FishPakagingManager.cs
@@ -1,11 +1,16 @@
+using System;
 using UnityEngine;
+using static packagingData;
 
 public class PakagingManager : MonoBehaviour
 {
+    [Header("Where to display the menu")]
     [SerializeField] public GameObject menuObject;
+    [Header("Animator")]
     [SerializeField] public Animator animator;
-
-    void Start()
+    private int[] flakeOil = {(int)OilType.Spicy, (int)OilType.Salt, (int)OilType.Mineral, (int)OilType.Shoyu, (int)OilType.Soy};
+    private int[] solidOil = {(int)OilType.Soy, (int)OilType.Olive, (int)OilType.SunFlower};
+    void Awake()
     {
         
     }
@@ -15,4 +20,24 @@ public class PakagingManager : MonoBehaviour
     {
         
     }
+}
+public static class packagingData
+{
+    public enum OilType
+    {
+        Spicy = 1,
+        Salt = 2,
+        Mineral = 3,
+        Shoyu = 4,
+        Soy = 5,
+        Olive = 6,
+        SunFlower = 7,
+    }
+}
+
+struct PackageRecipe
+{
+    bool isFlake;
+    int weight;
+    OilType oilType;
 }
