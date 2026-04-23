00b7c07a1eecb8cad8d5a7e2e23d6c3b1e448df4
japan0101 <japan.pon11@gmail.com>
2026-04-24 00:12:08 +0700

    Fix something

--- Assets/Scripts/Fish_prep/Tray_box.cs
+++ Assets/Scripts/Fish_prep/Tray_box.cs
@@ -10,6 +10,8 @@ public class Tray_Box : MonoBehaviour, IDropHandler
     private UnityEvent calledEvent;
     [SerializeField]
     private Fish_Prep_Handler Handler;
+    [Tooltip("For storing the placement of each guts objects.")]
+    [SerializeField] private RectTransform[] gutPlacement;
     public void OnDrop(PointerEventData eventData)
     {
         GameObject dropped = eventData.pointerDrag;
@@ -18,8 +20,11 @@ public class Tray_Box : MonoBehaviour, IDropHandler
         if (Guts.ValidateCut())
         {
             Debug.Log("Calling Event");
-            Guts.parentAfterDrag = transform;
-            Destroy(dropped);
+            dropped.transform.position = gutPlacement[Handler.Guts_Count].position;
+            dropped.transform.SetParent(gutPlacement[Handler.Guts_Count]);
+            Guts.enabled = false;
+            //Guts.parentAfterDrag = transform;
+            //Destroy(dropped);
             Handler.Guts_Count += 1;
         }
         if(Handler.Guts_Count == 5)
