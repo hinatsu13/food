37f17b29c78e1a9580d7937fa00979746ebe6946
Madino <Doomfist12341@hotmail.com>
2026-04-24 13:33:40 +0700

    Delay popup open and add target panel ref
    Add a one-frame delay before opening the popup so Popup_panel.Start() can finish: check_onclick.cs now includes using System.Collections, a serialized GameObject targetPanel, an OpenPanelDelayed() method and an OpenAfterFrame() coroutine that activates the target panel and yields one frame. Scene04_Fish_Thaw.unity updated to wire the targetPanel reference, change the button's onClick to call OpenPanelDelayed, and update several UI material references/fileIDs and related material property values.

--- Assets/Scripts/Fish_Thaw/check_onclick.cs
+++ Assets/Scripts/Fish_Thaw/check_onclick.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using System.Collections;
 
 public class check_onclick : MonoBehaviour
 {
     [SerializeField] int click;
+    [SerializeField] GameObject targetPanel;
     int click_count;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,6 +26,24 @@ public class check_onclick : MonoBehaviour
     {
         gameObject.SetActive(true);
     }
+
+    // Called by the Button's onClick instead of OpenPanel directly.
+    // Waits one frame so Popup_panel.Start() finishes before re-activating.
+    public void OpenPanelDelayed()
+    {
+        if (targetPanel != null)
+        {
+            StartCoroutine(OpenAfterFrame());
+        }
+    }
+
+    IEnumerator OpenAfterFrame()
+    {
+        targetPanel.SetActive(true);
+        yield return null;
+        targetPanel.SetActive(true);
+    }
+
     // Update is called once per frame
     void Update()
     {
